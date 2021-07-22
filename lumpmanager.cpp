6a120e9211c9186c539a639c9ca1190be17b7f09
nosoop <nosoop@users.noreply.github.com>
2021-07-22 05:01:36 -0700

    Handle line endings from any platform

--- lumpmanager.cpp
+++ lumpmanager.cpp
@@ -38,6 +38,44 @@
 static EntityLumpManager s_LumpManager;
 EntityLumpManager* lumpmanager = &s_LumpManager;
 
+/**
+ * Reads a string from the given input stream, up to, but excluding line ending characters.
+ *
+ * Function sourced from https://stackoverflow.com/a/6089413 (CC BY-SA 3.0)
+ * Name and indentation has been modified
+ */
+std::istream& getline_any_eol(std::istream& is, std::string& t) {
+	t.clear();
+
+	// The characters in the stream are read one-by-one using a std::streambuf.
+	// That is faster than reading them one-by-one using the std::istream.
+	// Code that uses streambuf this way must be guarded by a sentry object.
+	// The sentry object performs various tasks,
+	// such as thread synchronization and updating the stream state.
+
+	std::istream::sentry se(is, true);
+	std::streambuf* sb = is.rdbuf();
+
+	for(;;) {
+		int c = sb->sbumpc();
+		switch (c) {
+		case '\n':
+			return is;
+		case '\r':
+			if(sb->sgetc() == '\n')
+				sb->sbumpc();
+			return is;
+		case std::streambuf::traits_type::eof():
+			// Also handle the case when the last line has no line ending
+			if(t.empty())
+				is.setstate(std::ios::eofbit);
+			return is;
+		default:
+			t += (char)c;
+		}
+	}
+}
+
 void EntityLumpManager::Parse(const char* pMapEntities) {
 	// Pattern copied from alliedmodders/stripper-source/master/parser.cpp
 	static std::regex keyValueLine("\"([^\"]+)\"\\s+\"([^\"]+)\"");
@@ -49,8 +87,8 @@ void EntityLumpManager::Parse(const char* pMapEntities) {
 	EntityLumpEntry currentEntry;
 	std::smatch match;
 	
-	// TODO we need a custom getline that handles any combination of CR / LF.
-	for (std::string line; std::getline(mapEntities, line);) {
+	// TODO rework the parser logic
+	for (std::string line; !getline_any_eol(mapEntities, line).eof();) {
 		if (line == "{") {
 			// TODO we should just be able to assert that this is empty, right?
 			currentEntry.clear();
